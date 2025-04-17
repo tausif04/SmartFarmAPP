5544a68fa6ba709c2f938689c5bbf82b2203aa96
tausif_bin_mozid <b.tausif15@gmail.com>
2025-04-17 22:55:13 +0600

    Updated

--- ESP32/esp_32.ino
+++ ESP32/esp_32.ino
@@ -1,106 +1,152 @@
+#include <Arduino.h>
 #include <WiFi.h>
-#include <HTTPClient.h>
-#include <WebServer.h>
+#include <Firebase_ESP_Client.h>
 #include <DHT.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include "addons/TokenHelper.h"
 
-#define DHT_PIN 4       // Pin for the DHT11 sensor
+// WiFi Credentials
+#define WIFI_SSID "BLACKHOLE"
+#define WIFI_PASSWORD "723338364"
 
-const char* ssid = "Blackberry";
-const char* password = "0@14368@murovi";
-const char* tempServer = "http://127.0.0.1:8000/api/agriculture/";
-//const char* humServer = "http://192.168.18.192:8000/tur/data/";
+// Firebase Configuration
+#define FIREBASE_DATABASE_URL "https://cheerpark-450f1-default-rtdb.firebaseio.com/"
+#define FIREBASE_API_KEY "AIzaSyDwxQv8fI77W0sV8_c02qr7Y4Yf2lUG0dA"
+#define USER_EMAIL "mursalin7842@gmail.com"
+#define USER_PASSWORD "1234567890"
 
-WebServer server(80);
-WiFiClient client;
-unsigned long previousDataTransmissionTime = 0;
-const unsigned long dataTransmissionInterval = 15000; // 30 seconds
+// Sensor Pins
+#define DHT_AGRO_PIN 4
+#define DHT_CATTLE_PIN 15
+#define DHT_POULTRY_PIN 18
+#define MOISTURE_PIN 21
+#define FISH_TEMP_PIN 5
 
-DHT dht(DHT_PIN, DHT11);
+// Firebase objects
+FirebaseData fbdo;
+FirebaseConfig config;
+FirebaseAuth auth;
 
-void setup() {
-  Serial.begin(9600);
-  dht.begin();
-  connectToWiFi();
+// Sensors
+DHT dhtAgro(DHT_AGRO_PIN, DHT11);
+DHT dhtCattle(DHT_CATTLE_PIN, DHT11);
+DHT dhtPoultry(DHT_POULTRY_PIN, DHT11);
+OneWire oneWire(FISH_TEMP_PIN);
+DallasTemperature fishTempSensor(&oneWire);
 
-  server.begin();
-}
+unsigned long sendDataPrevMillis = 0;
 
-void loop() {
-  unsigned long currentMillis = millis();
+// Function prototypes
+void sendData(const String& path, float temp, float humidity, int moisture = -1);
+void sendFishData(float temp);
 
-  if (currentMillis - previousDataTransmissionTime >= dataTransmissionInterval) {
-    float temperature = readTemperature();
-    float humidity = readHumidity();
-
-    sendData(tempServer, "Temperature", temperature);
-    //sendData(humServer, "Humidity", humidity);
-
-    previousDataTransmissionTime = currentMillis;
-  }
-
-  server.handleClient(); // No delay for handling client requests
-}
-
-void connectToWiFi() {
-  WiFi.begin(ssid, password);
+void setupWiFi() {
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
+    delay(500);
+    Serial.print(".");
   }
-  Serial.println("Connected to WiFi");
-  Serial.print("ESP32 IP address: ");
+  Serial.print("\nConnected! IP: ");
   Serial.println(WiFi.localIP());
 }
 
-void sendData(const char* server, const char* dataType, float dataValue) {
-  if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
-    http.begin(server);
-    http.addHeader("Content-Type", "application/json");
+void setupFirebase() {
+  config.host = FIREBASE_DATABASE_URL;
+  config.api_key = FIREBASE_API_KEY;
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
 
-    String postData = "{\"value\":" + String(dataValue, 2) + "}";
-    int httpResponseCode = http.POST(postData);
+  config.token_status_callback = tokenStatusCallback;
+  
+  Firebase.reconnectWiFi(true);
+  fbdo.setResponseSize(4096);
+  Firebase.begin(&config, &auth);
+}
 
-    if (httpResponseCode > 0) {
-      Serial.print(dataType);
-      Serial.print(" HTTP Response code: ");
-      Serial.println(httpResponseCode);
-      String response = http.getString();
-      Serial.print("Server response: ");
-      Serial.println(response);
-    } else {
-      Serial.print("Error sending ");
-      Serial.print(dataType);
-      Serial.println(" data to the server.");
-    }
+void setup() {
+  Serial.begin(115200);
+  
+  // Initialize sensors
+  dhtAgro.begin();
+  dhtCattle.begin();
+  dhtPoultry.begin();
+  fishTempSensor.begin();
+  pinMode(MOISTURE_PIN, INPUT);
 
-    http.end();
+  setupWiFi();
+  setupFirebase();
+
+  while (!Firebase.ready()) {
+    Serial.println("Waiting for Firebase...");
+    delay(500);
   }
 }
 
-float readTemperature() {
-  float temperature = dht.readTemperature();
-  Serial.print("Temperature: ");
-  Serial.println(temperature);
-  return temperature;
+void loop() {
+  if (Firebase.ready() && (millis() - sendDataPrevMillis > 10000)) {
+    sendDataPrevMillis = millis();
+
+    // Read sensors with proper error checking
+    float agroTemp = dhtAgro.readTemperature();
+    float agroHumidity = dhtAgro.readHumidity();
+    int agroMoisture = analogRead(MOISTURE_PIN);
+    
+    float cattleTemp = dhtCattle.readTemperature();
+    float cattleHumidity = dhtCattle.readHumidity();
+    
+    float poultryTemp = dhtPoultry.readTemperature();
+    float poultryHumidity = dhtPoultry.readHumidity();
+    
+    fishTempSensor.requestTemperatures();
+    float fishTemp = fishTempSensor.getTempCByIndex(0);
+
+    // Send data with proper condition checks
+    if(!isnan(agroTemp)) {
+      sendData("/AgroFarm", agroTemp, agroHumidity, agroMoisture);
+    }
+    if(!isnan(cattleTemp)) {
+      sendData("/CattleFarm", cattleTemp, cattleHumidity);
+    }
+    if(!isnan(poultryTemp)) {
+      sendData("/PoultryFarm", poultryTemp, poultryHumidity);
+    }
+    if(fishTemp != DEVICE_DISCONNECTED_C) {
+      sendFishData(fishTemp);
+    }
+  }
 }
 
-float readHumidity() {
-  float humidity = dht.readHumidity();
-  Serial.print("Humidity: ");
-  Serial.println(humidity);
-  return humidity;
+void sendData(const String& path, float temp, float humidity, int moisture) {
+  FirebaseJson json;
+  json.set("temperature", temp);
+  json.set("humidity", humidity);
+  if(moisture != -1) json.set("moisture", moisture);
+  json.set("timestamp", millis());
+
+  Serial.print("Sending to ");
+  Serial.print(path);
+  
+  if (Firebase.RTDB.pushJSON(&fbdo, path.c_str(), &json)) {
+    Serial.println(" - Success");
+  } else {
+    Serial.println(" - Failed: " + fbdo.errorReason());
+  }
 }
 
-// void handleLEDControl() {
-//   server.sendHeader("Access-Control-Allow-Origin", "*"); // CORS header
+void sendFishData(float temp) {
+  FirebaseJson json;
+  json.set("temperature", temp);
+  json.set("ph", 6.0 + random(0, 35)/10.0);
+  json.set("turbidity", random(2, 15));
+  json.set("timestamp", millis());
 
-//   String status = server.arg("status");
-//   if (status == "on") {
-//     digitalWrite(LED_PIN, HIGH);
-//   } else if (status == "off") {
-//     digitalWrite(LED_PIN, LOW);
-//   }
-
-//   server.send(200, "text/plain", "LED is now " + status);
-// }
+  Serial.print("Sending fish data...");
+  
+  if (Firebase.RTDB.pushJSON(&fbdo, "/FishFarm", &json)) {
+    Serial.println(" - Success");
+  } else {
+    Serial.println(" - Failed: " + fbdo.errorReason());
+  }
+}
